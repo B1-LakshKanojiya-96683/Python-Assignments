0ac2f36310a7163604d76eb3026949a9643f454c
LakshKanojiya <153948242+LakshKanojiya@users.noreply.github.com>
2026-03-04 13:59:40 +0530

    Update Problem3.py

--- Assignments/Assignment 1/Problem3.py
+++ Assignments/Assignment 1/Problem3.py
@@ -6,35 +6,33 @@
 #floor division
 
 num = int(input("Enter the 4 digit number      "))
-backup_num=num
-if num >=1000  and num <=9999:
-    
+backup_num = num
+
+if 1000 <= num <= 9999:
     print("Now displaying the face value of each digit")
-    for x in range (3,-1,-1):
-          face_value=int(num/(10**x))
-          num = num - face_value*(10**x)
-          print(f"{face_value}")
-    
+    for x in range(3, -1, -1):
+        face_value = num // (10**x)
+        num = num - face_value * (10**x)
+        print(f"{face_value}")
+
     print("        ")
     print("Now printing the place value of each digit ")
     num = backup_num
-    for x in range (3,-1,-1):
-          fv=int(num/(10**x))
-          num = num - fv*(10**x)
-          place_value= fv*(10**x)
-          print(f"{place_value}")
-    
+    for x in range(3, -1, -1):
+        fv = num // (10**x)
+        num = num - fv * (10**x)
+        place_value = fv * (10**x)
+        print(f"{place_value}")
+
     print("     ")
     print("Now reversing the number and printing it")
-    
-    reverse =0
+    reverse = 0
     num = backup_num
-    for x in range (3,-1,-1):
-        last_digit= num%10
-        num=int(num/10)
-        reverse=reverse+last_digit*(10**x)
+    for x in range(3, -1, -1):
+        last_digit = num % 10
+        num = num // 10
+        reverse = reverse + last_digit * (10**x)
     print(f"The reversed number is   {reverse}")
 
-
 else:
-        print("This is not a four digit number please try again")
+    print("This is not a four digit number please try again")
